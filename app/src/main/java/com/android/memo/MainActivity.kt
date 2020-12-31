10e2d587e6d51421b4f974675d9775cfe3d366e8
Ren <coffeeaxce@gmail.com>
2020-12-31 10:47:19 +0900

    List作成

--- app/src/main/java/com/android/memo/MainActivity.kt
+++ app/src/main/java/com/android/memo/MainActivity.kt
@@ -2,8 +2,7 @@ package com.android.memo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.widget.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,19 +12,23 @@ class MainActivity : AppCompatActivity() {
 
         val saveButton: Button = findViewById(R.id.saveButton)
         val memoText: TextView = findViewById(R.id.memoText)
-        val memoView: TextView = findViewById(R.id.memoView)
+
         val database = MemoDatabase.getInstance(this)
         val memoDao = database.memoDao()
+        val memoList = memoDao.getAll()
+        val data = arrayListOf("0")
 
+        for (memo in memoList) {
+            data.add(memo.text.toString())
+        }
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,data)
+        val listView: ListView = findViewById(R.id.listView)
+        listView.adapter = adapter
 
         saveButton.setOnClickListener{
-
             val newMemo = Memo(0,memoText.text.toString())
             memoDao.insert(newMemo)
-
-            val memoList = memoDao.getAll()
-            memoView.text = memoList[memoList.size-1].text
-
         }
     }
 }
